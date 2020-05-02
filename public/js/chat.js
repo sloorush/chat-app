d91dff9931f12563e34c27db5ea60c4b41de44d8
r-ush <aarush.bhatt@gmail.com>
2020-05-02 21:43:38 +0530

    error handling js

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,7 @@ socket.on('message',(message)=>{
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
 
-    const message=document.querySelector('input').value
+    const message=e.target.elements.message
 
     socket.emit('sendMessage',message)
 })
