52af7fd0ddb65bf71262a6f33a4ffd04aad980c5
r-ush <aarush.bhatt@gmail.com>
2020-05-03 23:24:56 +0530

    added login frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,63 +11,66 @@ const $messages=document.querySelector('#messages')
 const messageTemplate=document.querySelector('#message-template').innerHTML
 const locationTemplate=document.querySelector('#location-message-template').innerHTML
 
+//options
+const {username,room}=qs.parse(location.search,{ignoreQueryPrefix:true})
+
 socket.on('message',(message)=>{
-    console.log(message)
-    const html=Mustache.render(messageTemplate,{
-        message:message.text,
-        createdAt:moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend',html)
+	console.log(message)
+	const html=Mustache.render(messageTemplate,{
+		message:message.text,
+		createdAt:moment(message.createdAt).format('h:mm a')
+	})
+	$messages.insertAdjacentHTML('beforeend',html)
 })
 
 socket.on('locationMessage',(message)=>{
-    console.log(message)
-    const html=Mustache.render(locationTemplate,{
-        url:message.url,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend',html)
+	console.log(message)
+	const html=Mustache.render(locationTemplate,{
+		url:message.url,
+		createdAt: moment(message.createdAt).format('h:mm a')
+	})
+	$messages.insertAdjacentHTML('beforeend',html)
 })
 
 $messageForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    // disable form
-    $messageFormButton.setAttribute('disabled','disabled')
-    const message=e.target.elements.message.value
+	e.preventDefault()
+	// disable form
+	$messageFormButton.setAttribute('disabled','disabled')
+	const message=e.target.elements.message.value
 
-    socket.emit('sendMessage',message,(error)=>{
-        // enable form
-        $messageFormButton.removeAttribute('disabled')
-        //clear input after message sent
-        $messageFormInput.value=''
-        //return focus on textbox after msg send
-        $messageFormInput.focus()
+	socket.emit('sendMessage',message,(error)=>{
+		// enable form
+		$messageFormButton.removeAttribute('disabled')
+		//clear input after message sent
+		$messageFormInput.value=''
+		//return focus on textbox after msg send
+		$messageFormInput.focus()
 
-        if(error){
-            return console.log(error)
-        }
-        console.log(message)
-    })
+		if(error){
+			return console.log(error)
+		}
+		console.log(message)
+	})
 })
 
 $sendLocationButton.addEventListener('click',()=>{
-    if(!navigator.geolocation){
-        return alert('geolocation is not suppoerted, pls update browser')
-    }
+	if(!navigator.geolocation){
+		return alert('geolocation is not suppoerted, pls update browser')
+	}
 
-    //disable location btn
-    $sendLocationButton.setAttribute('disabled','disabled')
+	//disable location btn
+	$sendLocationButton.setAttribute('disabled','disabled')
 
-    navigator.geolocation.getCurrentPosition((position)=>{
-        socket.emit('sendLocation',{
-            latitude:position.coords.latitude,
-            longitude:position.coords.longitude
-        },()=>{
+	navigator.geolocation.getCurrentPosition((position)=>{
+		socket.emit('sendLocation',{
+			latitude:position.coords.latitude,
+			longitude:position.coords.longitude
+		},()=>{
 
-            //enable location btn
-            $sendLocationButton.removeAttribute('disabled')
+			//enable location btn
+			$sendLocationButton.removeAttribute('disabled')
 
-            console.log('Location shared!')
-        })
-    })
+			console.log('Location shared!')
+		})
+	})
 })
