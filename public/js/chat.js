9738fc5ed3b102aff669d51994e399570daeedbc
r-ush <aarush.bhatt@gmail.com>
2020-05-03 13:23:01 +0530

    add func to avoid multiple clicking on the same message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,30 @@
 const socket=io()
 
+//elements
+const $messageForm=document.querySelector('#message-form')
+const $messageFormInput=document.querySelector('input')
+const $messageFormButton=document.querySelector('button')
+const $sendLocationButton=document.querySelector('#send-location')
+
+
 socket.on('message',(message)=>{
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
+$messageForm.addEventListener('submit',(e)=>{
     e.preventDefault()
-
+    // disable form
+    $messageFormButton.setAttribute('disabled','disabled')
     const message=e.target.elements.message.value
 
     socket.emit('sendMessage',message,(error)=>{
+        // enable form
+        $messageFormButton.removeAttribute('disabled')
+        //clear input after message sent
+        $messageFormInput.value=''
+        //return focus on textbox after msg send
+        $messageFormInput.focus()
+
         if(error){
             return console.log(error)
         }
@@ -17,16 +32,23 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
     })
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('geolocation is not suppoerted, pls update browser')
     }
+
+    //disable location btn
+    $sendLocationButton.setAttribute('disabled','disabled')
 
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         },()=>{
+
+            //enable location btn
+            $sendLocationButton.removeAttribute('disabled')
+            
             console.log('Location shared!')
         })
     })
